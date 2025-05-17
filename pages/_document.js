3b60fa65bd4a61a24d0c4caf186ee67e554c00b6
unknown <luiscesar.munoz.cervantes.upiit@gmail.com>
2025-05-17 17:28:30 -0600

    add script

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4497215621533514"
+     crossorigin="anonymous"></script>
+      </Head>
       <meta name="google-adsense-account" content="ca-pub-4497215621533514"></meta>
       <body className="antialiased">
         <Main />
