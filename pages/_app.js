7cbeff5148086d9e9bece79d036e8de820f51f64
cesar muñoz cervantes <luiscesar.munoz.cervantes.upiit@gmail.com>
2026-01-19 20:13:08 -0600

    destroi page

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,9 @@ import { UserProvider } from '@/context/UserContext';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 import Navbar from '@/components/Navbar';
+import '../styles/chaoSamg.module.css';
 
-export default function MyApp({ Component, pageProps }) {
+/* export default function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
       <Navbar />
@@ -15,4 +16,17 @@ export default function MyApp({ Component, pageProps }) {
       </Layout>
     </UserProvider>
   );
+} */
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+ <div className="errorcontainer">
+  <h1 className="errorcontainerh1">Error 404</h1>
+  <p className="errorcontainerp">Esta página no existe o dejó de recibir servicio.</p>
+</div>
+
+
+
+  );
 }
+
