4c17cb5f7114068e7f7d35ae5038173af3e28a43
unknown <luiscesar.munoz.cervantes.upiit@gmail.com>
2026-01-21 13:30:19 -0600

    add new functions

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import Layout from '../components/Layout';
 import Navbar from '@/components/Navbar';
 import '../styles/chaoSamg.module.css';
 
-/* export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
       <Navbar />
@@ -16,17 +16,7 @@ import '../styles/chaoSamg.module.css';
       </Layout>
     </UserProvider>
   );
-} */
-
-export default function MyApp({ Component, pageProps }) {
-  return (
- <div className="errorcontainer">
-  <h1 className="errorcontainerh1">Error 404</h1>
-  <p className="errorcontainerp">Esta página no existe o dejó de recibir servicio.</p>
-</div>
+} 
 
 
-
-  );
-}
 
